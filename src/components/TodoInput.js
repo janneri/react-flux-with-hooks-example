1c4e88af50b39a146878a63b8df8814f0e2d7904
Janne Rintanen <janne.rintanen@solita.fi>
2020-04-25 15:04:51 +0300

    Autofocus on input after rendering.

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -3,16 +3,28 @@ import React, {useState} from 'react';
 const TodoInput = ({insertTodo, disabled}) => {
     const [text, setText] = useState('');
 
+    const callbackRef = inputElement => {
+        if (inputElement) {
+            inputElement.focus();
+        }
+    };
+
+    const insertTodoAndClearInput = event => {
+        const text = event.target.value;
+        if (event.key === "Enter" && text) {
+            setText("");
+            insertTodo(text);
+        }
+    };
+
     return (
         <input
             type="text"
+            ref={callbackRef}
             className="todo-input"
             placeholder="What needs to be done?"
             onChange={event => setText(event.target.value)}
-            onBlur={event => {
-                setText("");
-                insertTodo(event.target.value)
-            }}
+            onKeyPress={insertTodoAndClearInput}
             value={text}
             disabled={disabled}
         />
